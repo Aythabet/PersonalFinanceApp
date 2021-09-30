1c7a8eec5599293e983f58ec42a9153121851408
Ayoub <ayoubthabet@gmail.com>
2021-09-30 20:44:27 +0100

    models update

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,6 @@
 class Transaction < ApplicationRecord
+  belongs_to :transaction
+
   validates :title, presence: true
   validates :account, presence: true
   validates :value, presence: true, numericality: { only_float: true }
